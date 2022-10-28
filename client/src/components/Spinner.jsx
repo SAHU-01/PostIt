f0a4854c2c99578d832d9c468e531b22d3fbd7f5
Ankita Sahu <71656941+SAHU-01@users.noreply.github.com>
2022-10-29 00:53:55 +0530

    Fixes and deployment -frontend

--- client/src/components/Spinner.jsx
+++ client/src/components/Spinner.jsx
@@ -4,7 +4,7 @@ import { Circles } from "react-loader-spinner";
 const Spinner = ({ message }) => {
   return (
     <div className="flex flex-col justify-center items-center w-full h-full">
-      <Circles color="#00BFFF" height={50} width={200} className="mt-2" />
+      <Circles color="#224B0C" height={50} width={200} className="mt-2" />
       <p className="text-lg text-center px-2 pt-5">{message}</p>
     </div>
   );
