f6a6e45f24c6c71bece4266fd6f7c54fdc392eb5
Ankita Sahu <71656941+SAHU-01@users.noreply.github.com>
2022-10-29 01:04:44 +0530

    Bug fix 1

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Routes, Route, useNavigate } from "react-router-dom";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import Home from "./container/Home";
